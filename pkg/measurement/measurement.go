3c8f063bc9d6d4e41a63436b4b84af3da5b2c0b6
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2019-10-16 14:05:12 +0300

    Added battery and acceleration information to measurement

--- pkg/measurement/measurement.go
+++ pkg/measurement/measurement.go
@@ -5,11 +5,15 @@ import "time"
 const Collection = "measurements"
 
 type Measurement struct {
-	Name        string    `json:"name" firestore:"name"`
-	MAC         string    `json:"mac" firestore:"mac"`
-	Timestamp   time.Time `json:"ts" firestore:"ts"`
-	Temperature float64   `json:"temperature" firestore:"temperature"`
-	Humidity    float64   `json:"humidity" firestore:"humidity"`
-	Pressure    float64   `json:"pressure" firestore:"pressure"`
-	ID          string    `json:"id" firestore:"-"`
+	Name          string    `json:"name" firestore:"name"`
+	MAC           string    `json:"mac" firestore:"mac"`
+	Timestamp     time.Time `json:"ts" firestore:"ts"`
+	Temperature   float64   `json:"temperature" firestore:"temperature"`
+	Humidity      float64   `json:"humidity" firestore:"humidity"`
+	Pressure      float64   `json:"pressure" firestore:"pressure"`
+	Battery       int       `json:"battery" firestore:"battery"`
+	AccelerationX int       `json:"acceleration_x" firestore:"acceleration_x"`
+	AccelerationY int       `json:"acceleration_y" firestore:"acceleration_y"`
+	AccelerationZ int       `json:"acceleration_z" firestore:"acceleration_z"`
+	ID            string    `json:"id" firestore:"-"`
 }
