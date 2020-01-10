deec74eb58c80b3528435322cbef0b505f3f4f98
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2020-01-10 14:07:46 +0200

    Removed Windows deploy script

--- cloudfunctions/receiver/deploy.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-gcloud functions deploy ^
-  receive_measurement ^
-  --entry-point ReceiveMeasurement ^
-  --runtime go111 ^
-  --trigger-resource ruuvitag-measurements ^
-  --trigger-event google.pubsub.topic.publish ^
-  --env-vars-file .env.yaml
