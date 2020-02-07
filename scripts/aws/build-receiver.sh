48da41e7e2f2b6e9d06b0d8ed736674f1fee66bd
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2020-02-07 11:57:38 +0200

    Support for updating AWS lambda receiver

--- scripts/aws/build-receiver.sh
+++ scripts/aws/build-receiver.sh
@@ -2,14 +2,29 @@
 
 set -e
 
-source .env
-
 GOOS=linux go build -o receiver ../../cmd/aws/receiver/main.go
 zip receiver.zip receiver
-aws lambda create-function \
-  --function-name receiver \
-  --runtime go1.x \
-  --zip-file fileb://receiver.zip \
-  --handler receiver \
-  --role "$ROLE"
+
+set +e
+
+source .env
+
+if [[ -z "${FUNCTION_NAME}" ]]; then
+  FUNCTION_NAME=receiver
+fi
+
+if [[ -z "${CREATE}" ]]; then
+  aws lambda update-function-code \
+    --function-name $FUNCTION_NAME \
+    --zip-file fileb://receiver.zip \
+    --publish
+else
+  aws lambda create-function \
+    --function-name $FUNCTION_NAME \
+    --runtime go1.x \
+    --zip-file fileb://receiver.zip \
+    --handler receiver \
+    --role $ROLE
+fi
+
 rm receiver receiver.zip
