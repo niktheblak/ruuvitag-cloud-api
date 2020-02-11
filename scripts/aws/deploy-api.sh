d28469317dcf4a1601af4bf235ee6e1a5005738d
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2020-02-11 16:50:10 +0200

    Script for deploying API as AWS lambda

--- scripts/aws/deploy-api.sh
+++ scripts/aws/deploy-api.sh
@@ -2,29 +2,29 @@
 
 set -e
 
-GOOS=linux go build -o receiver ../../cmd/aws/receiver/main.go
-zip receiver.zip receiver
+GOOS=linux go build -o api ../../cmd/aws/api/*.go
+zip api.zip api
 
 set +e
 
-source receiver.env
+source api.env
 
 if [[ -z "${FUNCTION_NAME}" ]]; then
-  FUNCTION_NAME=receiver
+  FUNCTION_NAME=api
 fi
 
 if [[ -z "${CREATE}" ]]; then
   aws lambda update-function-code \
     --function-name $FUNCTION_NAME \
-    --zip-file fileb://receiver.zip \
+    --zip-file fileb://api.zip \
     --publish
 else
   aws lambda create-function \
     --function-name $FUNCTION_NAME \
     --runtime go1.x \
-    --zip-file fileb://receiver.zip \
-    --handler receiver \
+    --zip-file fileb://api.zip \
+    --handler api \
     --role $ROLE
 fi
 
-rm receiver receiver.zip
+rm api api.zip
